f8ebfda28dc031a6a6b52ac57395bf6da9ec265c
Brian C. Ray <bcr666@gmail.com>
2025-08-14 09:17:21 -0500

    Project created with Maven

---
+++ src/main/java/io/github/bcr666/pocketsentinel/App.java
@@ -0,0 +1,13 @@
+package io.github.bcr666.pocketsentinel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/io/github/bcr666/pocketsentinel/AppTest.java
@@ -0,0 +1,20 @@
+package io.github.bcr666.pocketsentinel;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
